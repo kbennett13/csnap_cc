15a28163d5f4ca29c1ae3d3ec3d54f1b90735c9d
Charles H <chathaway@logrit.com>
2014-03-25 18:26:20 -0400

    Added in the ability to translate by height

--- src/blocks/translate_percent.js
+++ src/blocks/translate_percent.js
@@ -1,21 +1,40 @@
 (function () {
-    return function (percent) {
+    return function (percent, direction) {
         var dest, delta=radians(this.heading);
 
-        var width = Math.abs(this.left()-this.right());
-    
-        var newX = this.position().x + 
-            (width * percent/100) * this.scale;
-        var newY = this.position().y;
-        var dist = Math.sqrt(Math.pow(this.position().x-newX, 2));
+        var width = this.costume.contents.width * this.scale;
+        var height = this.costume.contents.height * this.scale;
 
-        if (percent >= 0) {
-            dest = this.position().distanceAngle(dist, this.heading);
+        var newX=0, newY=0, dist=0;
+        console.log(direction[0]);
+
+        if(direction[0] === 'height') {
+            newY = this.position().y +
+                (height * percent/100);
+            dist = Math.sqrt(Math.pow(this.position().y-newY, 2));
+
+            if (percent >= 0) {
+                dest = this.position().distanceAngle(dist, this.heading+90);
+            } else {
+                dest = this.position().distanceAngle(
+                    Math.abs(dist),
+                    (this.heading - 90)
+                );
+            }
+
         } else {
-            dest = this.position().distanceAngle(
-                Math.abs(dist),
-                (this.heading - 180)
-            );
+            newX = this.position().x + 
+                (width * percent/100);
+            dist = Math.sqrt(Math.pow(this.position().x-newX, 2));
+
+            if (percent >= 0) {
+                dest = this.position().distanceAngle(dist, this.heading);
+            } else {
+                dest = this.position().distanceAngle(
+                    Math.abs(dist),
+                    (this.heading - 180)
+                );
+            }
         }
         this.setPosition(dest);
         this.positionTalkBubble();
