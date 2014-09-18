8a86b5eed418a129499d60cd2b98b8622fd79ef2
hughed2 <hughed2@rpi.edu>
2014-09-18 03:04:36 -0400

    Applying costume color change
    Let's actually commit this now...

--- src/blocks/changeCostumeColor.js
+++ src/blocks/changeCostumeColor.js
@@ -3,7 +3,7 @@
 
 
 		if(!this.originalPixels || this.name != this.originalName) {
-            this.originalPixels = [];
+         this.originalPixels = [];
 			this.originalName = this.name
 
 		}
@@ -12,26 +12,29 @@
 				.getImageData(0, 0, this.costume.contents.width,
 					this.costume.contents.height);
 		}
-		this.costumeColor = num;
+      if(!this.costumeColor[this.getCostumeIdx()]) {
+         this.costumeColor[this.getCostumeIdx()] = new Color(0,0,0);
+      }
+
 		this.colorChange = true;
 		currentPixels = this.costume.contents.getContext('2d')
 			.getImageData(0, 0,
 				this.costume.contents.width, this.costume.contents.height);
-		var hsv = this.color.hsv();
+		var hsv = this.costumeColor[this.getCostumeIdx()].hsv();
 
 		hsv[0] = Math.max(Math.min(+num || 0, 100), 0) / 100;
 		hsv[1] = 1; // we gotta fix this at some time
-		this.color.set_hsv.apply(this.color, hsv);
+		this.costumeColor[this.getCostumeIdx()].set_hsv.apply(this.costumeColor[this.getCostumeIdx()], hsv);
 
 		for(var I = 0, L = this.originalPixels[this.getCostumeIdx()].data.length; I < L; I += 4){
 			if(currentPixels.data[I + 3] > 0){
 				// If it's not a transparent pixel
 				currentPixels.data[I] = this.originalPixels[this.getCostumeIdx()].
-					data[I] / 255 * this.color.r;
+					data[I] / 255 * this.costumeColor[this.getCostumeIdx()].r;
 				currentPixels.data[I + 1] = this.originalPixels[this.getCostumeIdx()].
-					data[I + 1] / 255 * this.color.g;
+					data[I + 1] / 255 * this.costumeColor[this.getCostumeIdx()].g;
 				currentPixels.data[I + 2] = this.originalPixels[this.getCostumeIdx()].
-					data[I + 2] / 255 * this.color.b;
+					data[I + 2] / 255 * this.costumeColor[this.getCostumeIdx()].b;
 			}
 		}
 		this.costume.contents.getContext('2d')
--- src/blocks/changeCostumeShade.js
+++ src/blocks/changeCostumeShade.js
@@ -1,9 +1,12 @@
 (function () {
 	return function (num) {
-		var hsv = this.costumeColor.hsv();
+      if(!this.costumeColor[this.getCostumeIdx()]) {
+         this.costumeColor[this.getCostumeIdx()] = new Color(0,0,0);
+      }
+		var hsv = this.costumeColor[this.getCostumeIdx()].hsv();
       
 		hsv[1] = 1; 
 		hsv[2] = Math.max(Math.min(+num || 0, 100), 0) / 100;
-		this.costumeColor.set_hsv.apply(this.costumeColor, hsv);
+		this.costumeColor[this.getCostumeIdx()].set_hsv.apply(this.costumeColor[this.getCostumeIdx()], hsv);
 	};
 }());
